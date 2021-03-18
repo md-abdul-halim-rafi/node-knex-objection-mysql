95a354abd6a40d1d74a633824dd38a7a3aa4b61b
Md-Abdul-Halim-Rafi <mdabdulhalimrafi@gmail.com>
2021-03-18 18:18:15 +0600

    dynamin knecfile

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,5 @@
 module.exports = {
-    test: {
+    [process.env.NODE_ENV]: {
         client: "mysql",
         connection: {
             host: "localhost",
@@ -13,30 +13,5 @@ module.exports = {
         seeds: {
             directory: __dirname + "/db/seeds/test",
         },
-    },
-    development: {
-        client: "mysql",
-        connection: {
-            host: "localhost",
-            user: "root", // replace with your mysql username
-            password: "123456", // replace with your mysql password
-            database: "objection_crud",
-        },
-        migrations: {
-            directory: __dirname + "/db/migrations",
-        },
-        seeds: {
-            directory: __dirname + "/db/seeds/development",
-        },
-    },
-    production: {
-        client: "pg",
-        connection: process.env.DATABASE_URL,
-        migrations: {
-            directory: __dirname + "/db/migrations",
-        },
-        seeds: {
-            directory: __dirname + "/db/seeds/production",
-        },
-    },
+    }
 };
